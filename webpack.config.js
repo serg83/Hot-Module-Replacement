10c892132b2aee99502757590ca176565cb9d400
Sergiu <sergiu.gaidau@gmail.com>
2019-05-18 22:31:31 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ const path = require('path');
 
 
 module.exports = {
-  module.exports = {
-  entry: './src/app.js',
+    entry: './src/app.js',
   output: {
     path: path.resolve(__dirname,'dist'),
     filename: 'app.bundle.js'
